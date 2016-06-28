bdc69e5e68ec7ba2f77d05957490c20b951c1a52
ywbshiwo <785534746@qq.com>
2016-06-28 12:09:58 +0800

    for good looking

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # maybe_lazy_1
 “懒人”养成计划-1
-#npm install
+#1:npm install
+
+#npm install 失败的同学，建议使用更换淘宝的npm源
+#具体可见：http://npm.taobao.org/
+
+#2:npm run http-server
+
+#3:npm run bs-server
--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var exec = require("child_process").exec;
+
 
 //编译sass,并放入到browsersync的流文件中
 gulp.task('sass', function() {
     return gulp.src('./sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'))
         .pipe(browserSync.stream())
+        .pipe(gulp.dest('./css'));
 });
 
 //监控sass变化
@@ -21,15 +23,14 @@ gulp.task('sass:watch', function() {
 gulp.task("browsersync-watch",function(){
     gulp.watch([
     	"./*.html",
-    	'./css/*.css',
-    	'./js/*.js'
+    	'./js/**/*.js'
 	]).on('change', browserSync.reload);
-
 });
+
+
 
 gulp.task("server",['sass:watch','browsersync-watch'],function(){
 	browserSync.init({
-		//在package.json配置的端口9090
         proxy: "127.0.0.1:9090"
     });
 });
